bea1e94ec74e17aca01a18bd48c09523eba9d50c
akashsathe063 <akashsathe063@gmail.com>
2022-06-20 18:31:34 +0530

    Emp attendence check program added

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,13 @@ package com.day4.start;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-
+        int isPresent=(int)Math.floor(Math.random()*10)%2;
+        if(isPresent==1)
+        {
+            System.out.println("Employee is present");
+        }
+       else{
+            System.out.println("Employe is absent");
+        }
     }
 }
