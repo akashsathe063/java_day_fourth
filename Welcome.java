8c220eb516c3c14756f2a947e03cd184b4e0be8c
akashsathe063 <akashsathe063@gmail.com>
2022-06-20 21:01:12 +0530

    Emp Wage by using Switch case

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,8 @@
 package com.day4.start;
 
 public class Welcome {
-    static int isFullTime=1;
-    static int isPartTime=2;
+   //static int isFullTime=1;
+  //  static int isPartTime=2;
     static int WagePerHour=20;
 
     static int FullDayHour=8;
@@ -11,20 +11,21 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int CheckRandom=(int)Math.floor(Math.random()*10)%3;
-        if(CheckRandom==isFullTime)
-        {
+        switch(CheckRandom){
+        case 1:
             System.out.println("Employee is present");
             Wage= WagePerHour*FullDayHour;
             System.out.println("Daily employee wage: "+Wage);
-        }
-        else if(CheckRandom==isPartTime)
-        {
-            System.out.println("Employee is part time present");
-            Wage=WagePerHour*PartTimeHours;
-            System.out.println("Part Time Employee Wage: "+Wage);
-        }
-       else{
-            System.out.println("Employe is absent");
+        break;
+            case 2:
+                System.out.println("Employee is partime");
+                Wage=WagePerHour*PartTimeHours;
+                System.out.println("part time Employee Wage: "+Wage);
+                break;
+            default :
+                System.out.println("Employee is absent");
+                
+
         }
     }
 }
